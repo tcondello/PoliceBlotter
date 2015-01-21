4d6080735fda2a292c3fffb482afb970695599db
TJC <condellotj@gmail.com>
2015-01-21 22:10:08 +0000

    codio

--- js/main.js
+++ js/main.js
@@ -5,12 +5,11 @@ var map;
 var ShapeNameLocation = 1;
 var ShapeName = new Array("json/CouncilDistrict.json", "json/Neighborhood.json", "json/PoliceZones.json");
 
-
 function toggleLayer(i){
-    shownLayer.setMap(null);
-    this_layer.setMap(map);
-    shownLayer = this_layer;
+    map.data.remove(ShapeName[ShapeNameLocation]);
     ShapeNameLocation = i;
+    map.data.loadGeoJson(ShapeName[ShapeNameLocation]);
+
 }
 function initialize() {
     map = new google.maps.Map(document.getElementById("map-canvas"), {
@@ -19,15 +18,18 @@ function initialize() {
         mapTypeId: 'roadmap'
 
     });
+    
     map.data.loadGeoJson(ShapeName[ShapeNameLocation]);
     map.data.setStyle({
             fillColor: 'green',
             strokeWeight: 1
         });
-
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
 //notes
+//
+//http://stackoverflow.com/questions/23071775/how-to-remove-arbitrary-layers-of-geojson-in-google-maps
+//
 //http://ec2-54-165-176-227.compute-1.amazonaws.com/PhpPittsburghBrigade/PoliceBlotterOutlineTest.html
 //    http://ec2-54-165-176-227.compute-1.amazonaws.com/PhpPittsburghBrigade/PoliceBlotterIndex.html
