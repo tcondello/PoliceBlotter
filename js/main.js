753565c50fe7ddf9be692b5c9484525424f2b184
TJC <condellotj@gmail.com>
2015-01-22 04:22:22 +0000

    layers

--- js/main.js
+++ js/main.js
@@ -2,28 +2,37 @@
  * Created by Mac-astr010 on 1/20/15.
  */
 var map;
-var ShapeNameLocation = 1;
-var ShapeName = new Array("json/CouncilDistrict.json", "json/Neighborhood.json", "json/PoliceZones.json");
-
-function toggleLayer(i){
-    map.data.remove(ShapeName[ShapeNameLocation]);
-    ShapeNameLocation = i;
-    map.data.loadGeoJson(ShapeName[ShapeNameLocation]);
-
+var style = {
+    fillColor: 'green',
+    strokeWeight: 1    
 }
+
+var Councilmap = new google.maps.Data();
+Councilmap.loadGeoJson('json/CouncilDistrict.json');
+var Neighbormap = new google.maps.Data();
+Neighbormap.loadGeoJson('json/Neighborhood.json');
+var PoliceZmap = new google.maps.Data();
+PoliceZmap.loadGeoJson('json/PoliceZones.json');
+
+function toggleLayer(this_layer){
+  Councilmap.setMap(null);
+  Neighbormap.setMap(null);
+  PoliceZmap.setMap(null);
+  this_layer.setStyle(style);
+  this_layer.setMap(map);
+}
+
 function initialize() {
     map = new google.maps.Map(document.getElementById("map-canvas"), {
         center: new google.maps.LatLng(40.435467, -79.996404),
         zoom: 12,
-        mapTypeId: 'roadmap'
+        mapTypeId: 'roadmap',
 
     });
     
-    map.data.loadGeoJson(ShapeName[ShapeNameLocation]);
-    map.data.setStyle({
-            fillColor: 'green',
-            strokeWeight: 1
-        });
+    Councilmap.setMap(map);
+    Councilmap.setStyle(style);
+    
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
