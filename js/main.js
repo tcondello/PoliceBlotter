0cd0275b323be0faedcfa538dcdb5f17d3552e34
TJC <condellotj@gmail.com>
2015-01-23 03:37:15 +0000

    more

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ var style = {
     fillColor: 'green',
     strokeWeight: 1    
 }
-
 var Councilmap = new google.maps.Data();
 Councilmap.loadGeoJson('json/CouncilDistrict.json');
 var Neighbormap = new google.maps.Data();
@@ -14,31 +13,59 @@ Neighbormap.loadGeoJson('json/Neighborhood.json');
 var PoliceZmap = new google.maps.Data();
 PoliceZmap.loadGeoJson('json/PoliceZones.json');
 
-function toggleLayer(this_layer){
+function changeActive(){
+  $(document).ready(function () {
+        $('ul.nav > li').click(function (e) {
+            e.preventDefault();
+            $('ul.nav > li').removeClass('active');
+            $(this).addClass('active');                
+        });            
+    });  
+}
+function setMouseover (layer){
+    layer.addListener('mouseover', function (event) {
+        layer.revertStyle();
+        layer.overrideStyle(event.feature, {
+              strokeColor: "#FF0000",
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: "#FF0000",
+              fillOpacity: 0.1
+        });
+    });
+    layer.addListener('mouseout', function (event) {
+        layer.revertStyle();
+    });
+}
+function clearLayer(){
   Councilmap.setMap(null);
   Neighbormap.setMap(null);
   PoliceZmap.setMap(null);
+}
+function toggleLayer(this_layer){ 
+  changeActive();
+  clearLayer();
   this_layer.setStyle(style);
   this_layer.setMap(map);
+  setMouseover(this_layer);
 }
-
 function initialize() {
     map = new google.maps.Map(document.getElementById("map-canvas"), {
         center: new google.maps.LatLng(40.435467, -79.996404),
         zoom: 12,
         mapTypeId: 'roadmap',
+    });    
 
-    });
-    
-    Councilmap.setMap(map);
-    Councilmap.setStyle(style);
-    
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
+
+
 //notes
 //
-//http://stackoverflow.com/questions/23071775/how-to-remove-arbitrary-layers-of-geojson-in-google-maps
 //
 //http://ec2-54-165-176-227.compute-1.amazonaws.com/PhpPittsburghBrigade/PoliceBlotterOutlineTest.html
 //    http://ec2-54-165-176-227.compute-1.amazonaws.com/PhpPittsburghBrigade/PoliceBlotterIndex.html
+   
+   
+   
